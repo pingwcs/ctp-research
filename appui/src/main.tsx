eb8aa3dc5d20b120eb26fb025ac2c315f4480e35
Charles <goletsz2019@gmail.com>
2026-07-08 01:32:46 +0800

    initiate react app

--- appui/src/main.tsx
+++ appui/src/main.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ConfigProvider, theme } from 'antd';
+import 'antd/dist/reset.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { ANT_THEME } from './config/theme';
 import { store } from './store';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ConfigProvider theme={{ ...ANT_THEME, algorithm: theme.darkAlgorithm }}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ConfigProvider>
   </React.StrictMode>,
 );
--- appui/src/store/index.ts
+++ appui/src/store/index.ts
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector, type TypedUseSelectorHook } from 'react-redux';
 
+import backtestReducer from './backtestSlice';
+import configReducer from './configSlice';
 import marketReducer from './marketSlice';
 
 export const store = configureStore({
   reducer: {
+    backtest: backtestReducer,
+    config: configReducer,
     market: marketReducer,
   },
 });

25f38bed70f26fb11d8b23279834130f6ba68a39
Charles <goletsz2019@gmail.com>
2026-07-08 16:39:19 +0800

    update package.json

--- appui/postcss.config.js
+++ appui/postcss.config.js
@@ -2,5 +2,10 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-pxtorem': {
+      rootValue: 16,
+      propList: ['*'],
+      exclude: /node_modules/i,
+    },
   },
 };

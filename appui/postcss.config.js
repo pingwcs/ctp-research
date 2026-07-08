8f94f24d4338e75d93257ff37e2d5122bcd37a4b
Charles <goletsz2019@gmail.com>
2026-07-08 21:12:32 +0800

    add postcss: px2rem config

--- appui/postcss.config.js
+++ appui/postcss.config.js
@@ -1,10 +1,17 @@
+const REM_ROOT_VALUE = 100;
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
     'postcss-pxtorem': {
-      rootValue: 16,
+      // Keep this value aligned with ROOT_FONT_BASE in src/config/responsive.ts.
+      rootValue: REM_ROOT_VALUE,
       propList: ['*'],
+      selectorBlackList: ['.norem'],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 1,
       exclude: /node_modules/i,
     },
   },
